d2e7f2f9adc18f4eabda6a03c128d8539a886520
unknown <MochammadArdiansyahh>
2021-06-09 22:08:05 +0700

    hore berhasil

---
+++ src/utama/Kedua.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utama;
+
+/**
+ *
+ * @author Moch Ardiansyah
+ */
+public class Kedua {
+    void identitas(){
+        String nama = "Moch Ardiansyah";
+        int nis = 6525;
+        
+        System.out.println("NAMA : "+nama);
+        System.out.println("NIS : "+nis);
+    }
+}
